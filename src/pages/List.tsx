6bffe81a9df9a5225c2c879cffb4303ba962fdc1
--unset <--unset>
2024-03-25 11:00:05 -0300

    adicionando ordenação de colunas

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -3,18 +3,48 @@ import { Link } from "react-router-dom";
 import { IProduct } from "../interfaces/IProduct";
 import ProductService from "../services/ProductService";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import StatusCard from "../components/StatusCard";
+
+enum SortingMode {
+	ID,
+	Name,
+	Price
+}
 
 function List() {
 	const [products, setProducts] = useState([] as IProduct[]);
 	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
+	const [status, setStatusCode] = useState<number>(200);
 	const [showingId, setShowingId] = useState(0);
 
+	function sortProducts(mode: SortingMode) {
+		let temp = Array.from(products);
+
+		switch (mode) {
+			case SortingMode.ID:
+				temp.sort((a, b) => a.id! - b.id!)
+				break;
+			case SortingMode.Name:
+				temp.sort((a, b) => a.name.localeCompare(b.name));
+				break;
+			case SortingMode.Price:
+				temp.sort((a, b) => a.price - b.price)
+				break;
+		}
+		setProducts(temp);
+	}
+
 	const fetchProducts = async () => {
+		setLoading(true);
+
 		const res = await ProductService.index();
 
+		setStatusCode(res.status);
 		if (res.status == 200) {
-			setProducts(res.data as IProduct[]);
+			setTimeout(() => {
+				setProducts(res.data as IProduct[]);
+				setLoading(false);
+			}, 1000)
 		}
 	}
 
@@ -29,6 +59,7 @@ function List() {
 
 	return (
 		<>
+			{!loading && <StatusCard duration={2000} statusCode={status} />}
 			<div className="min-h-full">
 				<div className="bg-gray-800 pb-32">
 					<header className="py-10">
@@ -48,7 +79,6 @@ function List() {
 
 				<main className="-mt-32">
 					<div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-						{/* Replace with your content */}
 						<div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
 							<div className="flex flex-col">
 								<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -61,19 +91,32 @@ function List() {
 															scope="col"
 															className="px-6 py-3 text-left text-xs font-bold text-gray-500 tracking-wider"
 														>
-															ID
+															<p onClick={() => {
+																sortProducts(SortingMode.ID)
+															}} className="w-fit">
+																ID
+															</p>
 														</th>
 														<th
 															scope="col"
 															className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider"
 														>
-															Nome
+															<p onClick={() => {
+																sortProducts(SortingMode.Name)
+															}} className="w-fit">
+
+																Nome
+															</p>
 														</th>
 														<th
 															scope="col"
 															className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider"
 														>
-															Preço
+															<p onClick={() => {
+																sortProducts(SortingMode.Price)
+															}} className="w-fit">
+																Preço
+															</p>
 														</th>
 														<th
 															scope="col"
@@ -84,6 +127,24 @@ function List() {
 													</tr>
 												</thead>
 												<tbody className="bg-white divide-y divide-gray-200">
+
+													{loading && <tr className="animate-pulse">
+														<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+															<div className="h-2.5 bg-gray-200 rounded-full w-6" />
+														</td>
+														<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+															<div className="h-2.5 bg-gray-200 rounded-full w-96" />
+														</td>
+														<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+															<div className="h-2.5 bg-gray-200 rounded-full w-12" />
+														</td>
+														<td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+															<div className="flex flex-row justify-end">
+																<div className="h-2.5 bg-gray-200 rounded-full w-20" />
+															</div>
+														</td>
+													</tr>}
+
 													{products.map((p) => (
 														<tr key={p.name}>
 															<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{p.id}</td>
@@ -96,7 +157,7 @@ function List() {
 																	{p.name}
 																</p>
 
-																{(showingId == p.id) && <div className="animate-pulse rounded-md bg-blue-50 p-4 w-fit absolute">
+																{(showingId == p.id) && <div className="animate-pulse rounded-md bg-blue-50 p-4 w-4/6 absolute">
 																	<div className="flex">
 																		<div className="flex-shrink-0">
 																			<InformationCircleIcon className="h-5 w-5 text-blue-400" aria-hidden="true" />
@@ -125,7 +186,6 @@ function List() {
 								</div>
 							</div>
 						</div>
-						{/* /End replace */}
 					</div>
 				</main>
 			</div>
